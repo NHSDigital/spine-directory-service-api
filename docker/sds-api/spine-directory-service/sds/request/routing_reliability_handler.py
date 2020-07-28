bb9a73de777ce8dd80e33fdce61e764dc5a07729
jaroslaw-wojciechowski <jaroslaww@kainos.com>
2020-07-28 12:48:57 +0200

    Feature/niad 425 sds api fhir compliant (#14)
    * NIAD-425: SDS API FHIR compliant

--- docker/sds-api/spine-directory-service/sds/request/routing_reliability_handler.py
+++ docker/sds-api/spine-directory-service/sds/request/routing_reliability_handler.py
@@ -1,4 +1,6 @@
 from request.base_handler import BaseHandler
+from request.content_type_validator import get_valid_accept_type
+from request.http_headers import HttpHeaders
 from utilities import timing, integration_adaptors_logger as log
 
 logger = log.IntegrationAdaptorsLogger(__name__)
@@ -11,6 +13,7 @@ class RoutingReliabilityRequestHandler(BaseHandler):
     async def get(self):
         org_code = self.get_query_argument("org-code")
         service_id = self.get_query_argument("service-id")
+        content_type = get_valid_accept_type(self.request.headers)
 
         logger.info("Looking up routing information. {org_code}, {service_id}",
                     fparams={"org_code": org_code, "service_id": service_id})
@@ -29,3 +32,4 @@ class RoutingReliabilityRequestHandler(BaseHandler):
                     fparams={"routing_reliability_information": combined_info})
 
         self.write(combined_info)
+        self.set_header(HttpHeaders.CONTENT_TYPE, content_type)
--- docker/sds-api/spine-directory-service/sds/request/tests/test_routing_handler.py
+++ docker/sds-api/spine-directory-service/sds/request/tests/test_routing_handler.py
@@ -3,10 +3,11 @@ import unittest.mock
 
 import tornado.testing
 import tornado.web
-from utilities import test_utilities
 
 from request import routing_handler
+from request.http_headers import HttpHeaders
 from request.tests import test_request_handler
+from utilities import test_utilities
 
 END_POINT_DETAILS = {"end_point": "http://www.example.com"}
 
@@ -26,6 +27,7 @@ class TestRoutingRequestHandler(tornado.testing.AsyncHTTPTestCase):
 
         self.assertEqual(response.code, 200)
         self.assertEqual(END_POINT_DETAILS, json.loads(response.body))
+        self.assertEqual(response.headers.get(HttpHeaders.CONTENT_TYPE, None), "application/json")
         self.routing.get_end_point.assert_called_with(test_request_handler.ORG_CODE, test_request_handler.SERVICE_ID)
 
     def test_get_returns_error(self):
@@ -50,5 +52,36 @@ class TestRoutingRequestHandler(tornado.testing.AsyncHTTPTestCase):
 
         with self.subTest("Missing Org Code & Service ID"):
             response = self.fetch(test_request_handler.build_url(org_code=None, service_id=None), method="GET")
+
+            self.assertEqual(response.code, 400)
+
+    def test_get_handles_different_accept_header(self):
+        with self.subTest("Accept header is missing"):
+            self.routing.get_end_point.return_value = test_utilities.awaitable(END_POINT_DETAILS)
+            response = self.fetch(test_request_handler.build_url(), method="GET")
+
+            self.assertEqual(response.code, 200)
+            self.assertEqual(response.headers.get(HttpHeaders.CONTENT_TYPE, None), "application/json")
+
+        with self.subTest("Accept header is application/fhir+json"):
+            headers = {'Accept': 'application/fhir+json'}
+            self.routing.get_end_point.return_value = test_utilities.awaitable(END_POINT_DETAILS)
+            response = self.fetch(test_request_handler.build_url(), method="GET", headers=headers)
+
+            self.assertEqual(response.code, 200)
+            self.assertEqual(response.headers.get(HttpHeaders.CONTENT_TYPE, None), "application/fhir+json")
+
+        with self.subTest("Accept header is application/fhir+xml"):
+            headers = {'Accept': 'application/fhir+xml'}
+            self.routing.get_end_point.return_value = test_utilities.awaitable(END_POINT_DETAILS)
+            response = self.fetch(test_request_handler.build_url(), method="GET", headers=headers)
+
+            self.assertEqual(response.code, 200)
+            self.assertEqual(response.headers.get(HttpHeaders.CONTENT_TYPE, None), "application/fhir+xml")
+
+        with self.subTest("Accept header is invalid"):
+            headers = {'Accept': 'invalid-header'}
+            self.routing.get_end_point.return_value = test_utilities.awaitable(END_POINT_DETAILS)
+            response = self.fetch(test_request_handler.build_url(), method="GET", headers=headers)
 
             self.assertEqual(response.code, 400)
