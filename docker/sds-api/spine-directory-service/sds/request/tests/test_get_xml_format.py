a6c8c69c68c61d8af1ce6836440761e4c3541ecd
jaroslaww <jaroslaww@kainos.com>
2020-08-18 11:24:14 +0200

    NIAD-505: clean-up

--- docker/sds-api/spine-directory-service/sds/request/tests/test_get_xml_format.py
+++ docker/sds-api/spine-directory-service/sds/request/tests/test_get_xml_format.py
@@ -8,9 +8,9 @@ from request.xml_formatter import get_xml_format
 class TestGetXmlFormat(TestCase):
 
     def test_get_xml_format(self):
-        print('---------------------------------- test: ')
         print(get_xml_format())
-        # tree = etree.parse(get_xml_format())
+
+    def test__xml_attributes(self):
         root = etree.fromstring(get_xml_format())
         attributes = root.attrib
         print(attributes)
