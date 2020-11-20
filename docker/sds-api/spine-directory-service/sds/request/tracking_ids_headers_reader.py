06c9af3e2544835402d8ac4b7890241766a91e93
Bartek Sarul <b.sarul@kainos.com>
2020-11-20 13:23:13 +0100

    NIAD-749: Checking X-Correlation-ID is an UUID

--- docker/sds-api/spine-directory-service/sds/request/tracking_ids_headers_reader.py
+++ docker/sds-api/spine-directory-service/sds/request/tracking_ids_headers_reader.py
@@ -1,9 +1,14 @@
+import re
+
+import tornado
 from tornado.httputil import HTTPHeaders
 
 from request.http_headers import HttpHeaders
 from utilities import message_utilities, mdc, integration_adaptors_logger as log
 
 logger = log.IntegrationAdaptorsLogger(__name__)
+
+UUID_PATTERN = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$"
 
 
 def read_tracking_id_headers(headers: HTTPHeaders):
@@ -16,4 +21,10 @@ def _extract_correlation_id(headers: HTTPHeaders):
     if not correlation_id:
         correlation_id = message_utilities.get_uuid()
         logger.info(f"Request is missing {HttpHeaders.X_CORRELATION_ID} header. Assigning new value: {correlation_id}")
+    else:
+        if len(re.findall(UUID_PATTERN, correlation_id)) != 1:
+            raise tornado.web.HTTPError(
+                status_code=400,
+                reason=f"Invalid {HttpHeaders.X_CORRELATION_ID} header. Should be an UUIDv4 matching regex '{UUID_PATTERN}'")
+
     mdc.correlation_id.set(correlation_id)
