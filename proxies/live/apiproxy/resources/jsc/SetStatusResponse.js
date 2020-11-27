ef448389876df9324ac7aef718dedaa195362aa1
Paul Walkington <paul.walkington@infinityworks.com>
2020-11-27 18:05:42 +0000

    NIAD-695 - add files

--- proxies/live/apiproxy/resources/jsc/SetStatusResponse.js
+++ proxies/live/apiproxy/resources/jsc/SetStatusResponse.js
@@ -1,43 +1,51 @@
 var apiproxy_revision = context.getVariable('apiproxy.revision');
-var healthcheck_service_response_code = context.getVariable('healthcheckResponse.status.code');
-var healthcheck_service_response = context.getVariable('healthcheckResponse.content');
-var healthcheck_service_request_url = context.getVariable('healthcheckRequest.url');
-var healthcheck_service_request_has_failed = context.getVariable("servicecallout.ServiceCallout.CallHealthcheckEndpoint.failed");
-var healthcheck_service_status = "fail";
 
-if(healthcheck_service_response_code/ 100 == 2){
-    healthcheck_service_status = "pass";
+var spine_response_code = context.getVariable('spineHealthcheckResponse.status.code');
+var spine_response = context.getVariable('spineHealthcheckResponse.content');
+var spine_request_url = context.getVariable('spineHealthcheckRequest.url');
+
+var spine_request_has_failed = context.getVariable("servicecallout.ServiceCallout.CallSpineHealthcheck.failed");
+
+var spine_status = "fail";
+
+if(spine_response_code/ 100 == 2){
+    spine_status = "pass";
 }
+
 timeout = "false";
 
-if(healthcheck_service_response_code === null && healthcheck_service_request_has_failed){
+if(spine_response_code === null && spine_request_has_failed){
     timeout = "true";
 }
 
-var healthcheck_service = {
-    "healthcheckService:status" : [
-        {
-            "status": healthcheck_service_status,
-            "timeout" : timeout,
-            "responseCode" : healthcheck_service_response_code,
-            "outcome": healthcheck_service_response,
-            "links" : {"self": healthcheck_service_request_url}
-        }
-    ]
+
+
+var spine_service = {
+"spine:status" : [
+    {
+    "status": spine_status, 
+    "timeout" : timeout,
+    "responseCode" : spine_response_code,
+    "outcome": spine_response,
+    "links" : {"self": spine_request_url}
+   }]
 };
 
 var apigee_status = "pass";
-if(healthcheck_service_status != "pass"){
+
+if(spine_status != "pass"){
     apigee_status = "fail";
 }
 
+
+
 var response = {  
-    "status" : apigee_status,
-    "version" : "{{ DEPLOYED_VERSION }}" ,
-    "revision" : apiproxy_revision, 
-    "releaseId" : "{{ RELEASE_RELEASEID }}", 
-    "commitId": "{{ SOURCE_COMMIT_ID }}",
-    "checks" : healthcheck_service
+"status" : apigee_status,
+"version" : "{{ DEPLOYED_VERSION }}" ,
+"revision" : apiproxy_revision, 
+"releaseId" : "{{ RELEASE_RELEASEID }}", 
+"commitId": "{{ SOURCE_COMMIT_ID }}",
+"checks" : spine_service
 };
 
 context.setVariable("status.response", JSON.stringify(response));
