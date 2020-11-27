8c21d478266aa54348fcdaf65255fd0ccc1c86b0
Bartek Sarul <b.sarul@kainos.com>
2020-11-27 15:33:05 +0100

    NIAD-695: Proxy definition adjustments

--- proxies/live/apiproxy/resources/jsc/SetStatusResponse.js
+++ proxies/live/apiproxy/resources/jsc/SetStatusResponse.js
@@ -1,53 +1,53 @@
 var apiproxy_revision = context.getVariable('apiproxy.revision');
-
-var spine_response_code = context.getVariable('spineHealthcheckResponse.status.code');
-var spine_response = context.getVariable('spineHealthcheckResponse.content');
-var spine_request_url = context.getVariable('spineHealthcheckRequest.url');
-
-var spine_request_has_failed = context.getVariable("servicecallout.ServiceCallout.CallSpineHealthcheck.failed");
-
-var spine_status = "fail";
-
-if(spine_response_code/ 100 == 2){
-    spine_status = "pass";
+ 
+var healthcheck_service_response_code = context.getVariable('healthcheckResponse.status.code');
+var healthcheck_service_response = context.getVariable('healthcheckResponse.content');
+var healthcheck_service_request_url = context.getVariable('healthcheckRequest.url');
+ 
+var healthcheck_service_request_has_failed = context.getVariable("servicecallout.ServiceCallout.CallHealthcheckEndpoint.failed");
+ 
+var healthcheck_service_status = "fail";
+ 
+if(healthcheck_service_response_code/ 100 == 2){
+    healthcheck_service_status = "pass";
 }
-
+ 
 timeout = "false";
-
-if(spine_response_code === null && spine_request_has_failed){
+ 
+if(healthcheck_service_response_code === null && healthcheck_service_request_has_failed){
     timeout = "true";
 }
-
-
-
-var spine_service = {
-"spine:status" : [
+ 
+ 
+ 
+var healthcheck_service = {
+"healthcheckService:status" : [
     {
-    "status": spine_status, 
+    "status": healthcheck_service_status,
     "timeout" : timeout,
-    "responseCode" : spine_response_code,
-    "outcome": spine_response,
-    "links" : {"self": spine_request_url}
+    "responseCode" : healthcheck_service_response_code,
+    "outcome": healthcheck_service_response,
+    "links" : {"self": healthcheck_service_request_url}
    }]
 };
-
+ 
 var apigee_status = "pass";
-
-if(spine_status != "pass"){
+ 
+if(healthcheck_service_status != "pass"){
     apigee_status = "fail";
 }
-
-
-
-var response = {  
+ 
+ 
+ 
+var response = { 
 "status" : apigee_status,
-"version" : "{{ DEPLOYED_VERSION }}" ,
-"revision" : apiproxy_revision, 
-"releaseId" : "{{ RELEASE_RELEASEID }}", 
-"commitId": "{{ SOURCE_COMMIT_ID }}",
-"checks" : spine_service
+"version" : "personal-demographics-pr-439" ,
+"revision" : apiproxy_revision,
+"releaseId" : "13860",
+"commitId": "f9a43166194ef761428a3507ae0e5244440a8bb0",
+"checks" : healthcheck_service
 };
-
+ 
 context.setVariable("status.response", JSON.stringify(response));
 context.setVariable("response.content", JSON.stringify(response));
 context.setVariable("response.header.Content-Type", "application/json");
