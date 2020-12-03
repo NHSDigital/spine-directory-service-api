003a9c1e7a724677eddd9c14d42c98c49a9d05b3
Bartek Sarul <b.sarul@kainos.com>
2020-12-03 09:34:26 +0100

    NIAD-706: Allow multiple accept headers

--- docker/sds-api/spine-directory-service/sds/request/content_type_validator.py
+++ docker/sds-api/spine-directory-service/sds/request/content_type_validator.py
@@ -6,20 +6,21 @@ from utilities import integration_adaptors_logger as log
 
 logger = log.IntegrationAdaptorsLogger(__name__)
 
+ANY = '*/*'
 APPLICATION_JSON = 'application/json'
 APPLICATION_FHIR_JSON = 'application/fhir+json'
 
-valid_accept_types = [APPLICATION_JSON, APPLICATION_FHIR_JSON]
-
 
 def get_valid_accept_type(headers: HTTPHeaders):
-    accept_type = headers.get(HttpHeaders.ACCEPT, APPLICATION_FHIR_JSON).lower()
+    accept_types = headers.get(HttpHeaders.ACCEPT, APPLICATION_FHIR_JSON).lower()
+    accept_types = accept_types.split(",")
+    accept_types = list(map(lambda value: value.strip(), accept_types))
 
-    if accept_type == '*/*':
+    if ANY in accept_types or APPLICATION_FHIR_JSON in accept_types:
         return APPLICATION_FHIR_JSON
-    elif accept_type in valid_accept_types:
-        return accept_type
+    elif APPLICATION_JSON in accept_types:
+        return APPLICATION_JSON
     else:
         logger.info("Invalid Accept header in request")
         raise tornado.web.HTTPError(status_code=406,
-                                    log_message=f'Invalid Accept header in request, only: {valid_accept_types} are allowed')
+                                    log_message=f'Invalid Accept header in request, only: {APPLICATION_JSON} or {APPLICATION_FHIR_JSON} are allowed')
