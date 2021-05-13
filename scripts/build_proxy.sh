91e31c193c6d1db918fe883d79c4c469e6c036ba
Bartek Sarul <b.sarul@kainos.com>
2021-05-13 13:22:01 +0200

    NIAD-1292: Init test

--- scripts/build_proxy.sh
+++ scripts/build_proxy.sh
@@ -8,6 +8,5 @@ set -o nounset errexit pipefail
 rm -rf build/proxies
 mkdir -p build/proxies/sandbox
 mkdir -p build/proxies/live
-# cp -Rv proxies/sandbox/apiproxy build/proxies/sandbox
-cp -Rv proxies/live/apiproxy build/proxies/sandbox
+cp -Rv proxies/sandbox/apiproxy build/proxies/sandbox
 cp -Rv proxies/live/apiproxy build/proxies/live
