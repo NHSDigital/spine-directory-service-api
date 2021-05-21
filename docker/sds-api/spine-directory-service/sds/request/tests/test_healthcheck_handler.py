53be2fd7cb4fded4bf46273b4a59ebd8dc609b70
Bartek Sarul <b.sarul@kainos.com>
2021-05-21 11:21:44 +0200

    NIAD-1292: Unit tests fix

--- docker/sds-api/spine-directory-service/sds/request/tests/test_healthcheck_handler.py
+++ docker/sds-api/spine-directory-service/sds/request/tests/test_healthcheck_handler.py
@@ -13,9 +13,14 @@ FAIL = 'fail'
 
 class TestHealthcheckHandler(tornado.testing.AsyncHTTPTestCase):
 
+    def test_success_health(self):
+        response = self.fetch('/healthcheck', method='GET')
+
+        self.assertEqual(200, response.code)
+
     @patch('request.healthcheck_handler.config')
     @patch('request.healthcheck_handler.get_sds_client')
-    def test_success_health(self, mock_get_sds_client, mock_config):
+    def test_success_deep_healthcheck(self, mock_get_sds_client, mock_config):
         mock_config.get_config.return_value = LDAP_URL
 
         empty_future = asyncio.Future()
@@ -24,7 +29,7 @@ class TestHealthcheckHandler(tornado.testing.AsyncHTTPTestCase):
         mock_get_mhs_details.get_mhs_details.return_value = empty_future
         mock_get_sds_client.return_value = mock_get_mhs_details
 
-        response = self.fetch('/healthcheck', method='GET')
+        response = self.fetch('/healthcheck/deep', method='GET')
 
         self.assertEqual(200, response.code)
         response_body = json.loads(response.body.decode())
@@ -35,14 +40,14 @@ class TestHealthcheckHandler(tornado.testing.AsyncHTTPTestCase):
 
     @patch('request.healthcheck_handler.config')
     @patch('request.healthcheck_handler.get_sds_client')
-    def test_failure_health(self, mock_get_sds_client, mock_config):
+    def test_failure_deep_healthcheck(self, mock_get_sds_client, mock_config):
         mock_config.get_config.return_value = LDAP_URL
 
         mock_get_mhs_details = MagicMock()
         mock_get_mhs_details.get_mhs_details.side_effect = RuntimeError('some error')
         mock_get_sds_client.return_value = mock_get_mhs_details
 
-        response = self.fetch('/healthcheck', method='GET')
+        response = self.fetch('/healthcheck/deep', method='GET')
 
         self.assertEqual(503, response.code)
         response_body = json.loads(response.body.decode())
@@ -52,4 +57,8 @@ class TestHealthcheckHandler(tornado.testing.AsyncHTTPTestCase):
         self.assertEqual('some error', response_body['details']['ldap']['output'])
 
     def get_app(self) -> Application:
-        return tornado.web.Application([(r'/healthcheck', healthcheck_handler.HealthcheckHandler)])
+        return tornado.web.Application(
+            [
+                (r'/healthcheck', healthcheck_handler.HealthcheckHandler),
+                (r'/healthcheck/deep', healthcheck_handler.DeepHealthcheckHandler),
+            ])
