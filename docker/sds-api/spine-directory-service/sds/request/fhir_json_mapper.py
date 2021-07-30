9d5f935614a151eb3b72d231120ad8f01553cb37
Bartek Sarul <b.sarul@kainos.com>
2021-07-30 07:47:47 +0200

    NIAD-1634: Lower case access key. MHS test data

--- docker/sds-api/spine-directory-service/sds/request/fhir_json_mapper.py
+++ docker/sds-api/spine-directory-service/sds/request/fhir_json_mapper.py
@@ -34,6 +34,11 @@ def _map_resource_to_bundle_entry(resource, base_url):
 
 
 def build_endpoint_resources(ldap_attributes: Dict) -> List[Dict]:
+    ldap_attributes = ldap_attributes.copy()
+    for key in list(ldap_attributes.keys()):
+        ldap_attributes[key.lower()] = ldap_attributes[key]
+        del ldap_attributes[key]
+
     def build_endpoint(address):
         result = {
             "resourceType": "Endpoint",
@@ -44,9 +49,9 @@ def build_endpoint_resources(ldap_attributes: Dict) -> List[Dict]:
             "address": address
         }
 
-        managing_organization = _build_managing_organization(ldap_attributes.get("nhsIDCode"))
+        managing_organization = _build_managing_organization(ldap_attributes.get("nhsIDCode".lower()))
         if managing_organization:
-            result["managingOrganization"] = managing_organization
+            result["managingOrganization".lower()] = managing_organization
 
         identifiers = _build_identifier_array(ldap_attributes)
         identifiers = list(filter(lambda item: item, identifiers))
@@ -61,7 +66,7 @@ def build_endpoint_resources(ldap_attributes: Dict) -> List[Dict]:
                 "url": Url.EXTENSION_URL,
                 "extension": reliability_configuration_extensions
             })
-        interaction_id = ldap_attributes.get("nhsMhsSvcIA")
+        interaction_id = ldap_attributes.get("nhsMhsSvcIA".lower())
         if interaction_id:
             extensions.append(_build_value_reference_extension(
                 Url.SDS_SERVICE_INTERACTION_ID_URL, SERVICE_ID_FHIR_IDENTIFIER, interaction_id))
@@ -70,43 +75,48 @@ def build_endpoint_resources(ldap_attributes: Dict) -> List[Dict]:
             result["extension"] = extensions
 
         return result
-    return [build_endpoint(address) for address in ldap_attributes['nhsMHSEndPoint']]
+    return [build_endpoint(address) for address in ldap_attributes['nhsMHSEndPoint'.lower()]]
 
 
 def build_device_resource(ldap_attributes: Dict) -> Dict:
+    ldap_attributes = ldap_attributes.copy()
+    for key in list(ldap_attributes.keys()):
+        ldap_attributes[key.lower()] = ldap_attributes[key]
+        del ldap_attributes[key]
+
     device = {
         "resourceType": "Device",
         "id": message_utilities.get_uuid()
     }
 
     identifiers = []
-    unique_identifier = ldap_attributes.get('uniqueIdentifier')
+    unique_identifier = ldap_attributes.get('uniqueIdentifier'.lower())
     if len(unique_identifier) > 1:
         raise ValueError("LDAP returned more than 1 'uniqueIdentifier' attribute")
     unique_identifier = (unique_identifier or [None])[0]
     if unique_identifier:
         identifiers.append(build_identifier(Url.NHS_SPINE_ASID, unique_identifier))
-    party_key = ldap_attributes.get('nhsMhsPartyKey')
+    party_key = ldap_attributes.get('nhsMhsPartyKey'.lower())
     if party_key:
         identifiers.append(build_identifier(Url.NHS_MHS_PARTYKEY_URL, party_key))
     if identifiers:
         device['identifier'] = identifiers
 
     extension = []
-    manufacturing_organization = ldap_attributes.get('nhsIdCode')
+    manufacturing_organization = ldap_attributes.get('nhsIdCode'.lower())
     if manufacturing_organization:
         extension.append(
             _build_value_reference_extension(
                 Url.MANUFACTURING_ORGANIZATION_EXTENSION_URL, Url.MANUFACTURING_ORGANIZATION_URL, manufacturing_organization))
     service_id_extensions = list(map(
         lambda v: _build_value_reference_extension(Url.SDS_SERVICE_INTERACTION_ID_URL, SERVICE_ID_FHIR_IDENTIFIER, v),
-        ldap_attributes.get('nhsAsSvcIA')))
+        ldap_attributes.get('nhsAsSvcIA'.lower())))
     if service_id_extensions:
         extension += service_id_extensions
     if extension:
         device['extension'] = extension
 
-    client_id = (ldap_attributes.get('nhsAsClient') or [None])[0]
+    client_id = (ldap_attributes.get('nhsAsClient'.lower()) or [None])[0]
     if client_id:
         device["owner"] = {
             "identifier": {
@@ -119,26 +129,26 @@ def build_device_resource(ldap_attributes: Dict) -> Dict:
 
 
 def _build_identifier_array(ldap_attributes: Dict):
-    unique_identifiers = ldap_attributes.get("uniqueIdentifier")
+    unique_identifiers = ldap_attributes.get("uniqueIdentifier".lower())
     if len(unique_identifiers) > 1:
         raise ValueError("LDAP returned more than 1 'uniqueIdentifier' attribute")
 
     return [
-        build_identifier(Url.NHS_MHS_FQDN_URL, ldap_attributes.get("nhsMhsFQDN")),
-        build_identifier(Url.NHS_MHS_PARTYKEY_URL, ldap_attributes.get("nhsMHSPartyKey")),
-        build_identifier(Url.NHS_MHS_CPAID_URL, ldap_attributes.get("nhsMhsCPAId")),
+        build_identifier(Url.NHS_MHS_FQDN_URL, ldap_attributes.get("nhsMhsFQDN".lower())),
+        build_identifier(Url.NHS_MHS_PARTYKEY_URL, ldap_attributes.get("nhsMHSPartyKey".lower())),
+        build_identifier(Url.NHS_MHS_CPAID_URL, ldap_attributes.get("nhsMhsCPAId".lower())),
         build_identifier(Url.NHS_MHS_ID, (unique_identifiers or [None])[0])
     ]
 
 
 def _build_extension_array(ldap_attributes: Dict):
     return [
-        _build_string_extension("nhsMHSSyncReplyMode", ldap_attributes.get("nhsMHSSyncReplyMode")),
-        _build_string_extension("nhsMHSRetryInterval", ldap_attributes.get("nhsMHSRetryInterval")),
-        _build_int_extension("nhsMHSRetries", ldap_attributes.get("nhsMHSRetries")),
-        _build_string_extension("nhsMHSPersistDuration", ldap_attributes.get("nhsMHSPersistDuration")),
-        _build_string_extension("nhsMHSDuplicateElimination", ldap_attributes.get("nhsMHSDuplicateElimination")),
-        _build_string_extension("nhsMHSAckRequested", ldap_attributes.get("nhsMHSAckRequested"))
+        _build_string_extension("nhsMHSSyncReplyMode", ldap_attributes.get("nhsMHSSyncReplyMode".lower())),
+        _build_string_extension("nhsMHSRetryInterval", ldap_attributes.get("nhsMHSRetryInterval".lower())),
+        _build_int_extension("nhsMHSRetries", ldap_attributes.get("nhsMHSRetries".lower())),
+        _build_string_extension("nhsMHSPersistDuration", ldap_attributes.get("nhsMHSPersistDuration".lower())),
+        _build_string_extension("nhsMHSDuplicateElimination", ldap_attributes.get("nhsMHSDuplicateElimination".lower())),
+        _build_string_extension("nhsMHSAckRequested", ldap_attributes.get("nhsMHSAckRequested".lower()))
     ]
 
 
