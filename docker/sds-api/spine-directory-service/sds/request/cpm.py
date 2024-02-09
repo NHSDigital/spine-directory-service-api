09f4974633d30c0e0c2de1bc8d2d007c2c3db1f9
jameslinnell <james.linnell2@nhs.net>
2024-02-09 10:30:50 +0000

    Add arguments to returned list

--- docker/sds-api/spine-directory-service/sds/request/cpm.py
+++ docker/sds-api/spine-directory-service/sds/request/cpm.py
@@ -1,7 +1,20 @@
 from typing import List
 
 async def get_device_from_cpm(ods_code: str, interaction_id: str, manufacturing_organization: str = None, party_key: str = None) -> List:
-    return []
+    return [
+        {
+            "ods-code": ods_code,
+            "interaction_id": interaction_id,
+            "manufacturing_organisation": manufacturing_organization,
+            "party_key": party_key
+        }
+    ]
 
 async def get_endpoint_from_cpm(ods_code: str, interaction_id: str = None, party_key: str = None) -> List:
-    return []
+    return [
+        {
+            "ods-code": ods_code,
+            "interaction_id": interaction_id,
+            "party_key": party_key
+        }
+    ]
