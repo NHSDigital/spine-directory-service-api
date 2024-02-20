9b493724373bcdd497d31bedc7555c7aab448369
jameslinnell <jimlinnell@gmail.com>
2024-02-20 14:20:09 +0000

    [feature/PI-238] Filter and translate CPM data (Device) (#268)
    * filter and translate CPM data
    
    * Add more tests
    
    * Make the code nicer
    
    * Remove stray print

--- docker/sds-api/spine-directory-service/sds/request/cpm.py
+++ docker/sds-api/spine-directory-service/sds/request/cpm.py
@@ -1,4 +1,7 @@
+import copy
+
 from typing import List
+from lookup.sds_exception import SDSException
 
 async def get_device_from_cpm(ods_code: str, interaction_id: str, manufacturing_organization: str = None, party_key: str = None) -> List:
     return [
@@ -18,3 +21,95 @@ async def get_endpoint_from_cpm(ods_code: str, interaction_id: str = None, party
             "party_key": party_key
         }
     ]
+
+def filter_cpm_devices_response(data: dict, query_parts: dict):
+    if "org_code" not in query_parts or "interaction_id" not in query_parts or not query_parts["org_code"] or not query_parts["interaction_id"]:
+            raise SDSException("org_code and interaction_id must be provided")
+
+    filtered_results = []
+    filters = {
+        "org_code": False,
+        "service_id": False,
+    }
+    if "manufacturing_organization" in query_parts:
+        filters["manufact_org"] = False
+    if "party_key" in query_parts:
+        filters["party_key"] = False
+        
+    for result in data["entry"]:
+        if "resourceType" in result and result["resourceType"] == "Bundle":
+            for index, res in enumerate(result["entry"]):
+                if "resourceType" in res and res["resourceType"] == "QuestionnaireResponse":
+                    for service in res["item"]:
+                        if service["text"] == "Owner":
+                            for answer in service["answer"]:
+                                if answer["valueString"] == query_parts["org_code"]:
+                                    filters["org_code"] = True
+                        if service["text"] == "InteractionIds":
+                            for answer in service["answer"]:
+                                if answer["valueString"] == query_parts["interaction_id"]:
+                                    filters["service_id"] = True
+                                    break
+                        if "manufacturing_organization" in query_parts and service["text"] == "ManufacturingOdsCode":
+                            for answer in service["answer"]:
+                                if answer["valueString"] == query_parts["manufacturing_organization"]:
+                                    filters["manufact_org"] = True
+                                    break
+                        if "party_key" in query_parts and service["text"] == "PartyKey":
+                            for answer in service["answer"]:
+                                if answer["valueString"] == query_parts["party_key"]:
+                                    filters["party_key"] = True
+                                    break
+        all_filters_true = all(value for value in filters.values())
+        if all_filters_true:
+            filtered_results.append(result["entry"])
+        filters = {key: False for key in filters}
+                    
+    return filtered_results
+
+def transform_device_to_SDS(data: List) -> List:
+    ldap_data = []
+    default_data_dict = dict(
+        nhsAsClient = [],
+        nhsAsSvcIA = [],
+        nhsMhsManufacturerOrg = "",
+        nhsMhsPartyKey = "",
+        nhsIdCode = "",
+        uniqueIdentifier = ""
+    )
+
+    for d in data:
+        data_dict = copy.deepcopy(default_data_dict)
+        for item in d:
+            if "resource" in item:
+                data_dict["uniqueIdentifier"] = process_device_resource(item["resource"])
+            else:
+                data_dict = process_questionnaire_response(item, data_dict)
+
+        ldap_data.append(data_dict)
+
+    return ldap_data
+
+
+def process_device_resource(item):
+    if "resourceType" in item and item["resourceType"] == "Device":
+        if "identifier" in item:
+            for identifier in item["identifier"]:
+                if identifier.get("system") == "https://fhir.nhs.uk/Id/nhsSpineASID":
+                    return identifier.get("value")
+    return ""
+
+def process_questionnaire_response(item, data_dict):
+    if "resourceType" in item and item["resourceType"] == "QuestionnaireResponse":
+        for service in item.get("item", []):
+            if service["text"] == "InteractionIds":
+                for answer in service["answer"]:
+                    data_dict["nhsAsSvcIA"].append(answer["valueString"])
+            elif service["text"] == "ManufacturingOdsCode":
+                data_dict["nhsMhsManufacturerOrg"] = service["answer"][0].get("valueString", "")
+            elif service["text"] == "PartyKey":
+                data_dict["nhsMhsPartyKey"] = service["answer"][0].get("valueString", "")
+            elif service["text"] == "Owner":
+                data_dict["nhsAsClient"] = [service["answer"][0].get("valueString", "")]
+                data_dict["nhsIdCode"] = service["answer"][0].get("valueString", "")
+    return data_dict
