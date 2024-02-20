780726bcf94bf83b1fe88d5df89534b2bd80b4c5
jameslinnell <james.linnell2@nhs.net>
2024-02-20 17:02:42 +0000

    Added Endpoint filtering and cleaned up the code

--- docker/sds-api/spine-directory-service/sds/request/cpm.py
+++ docker/sds-api/spine-directory-service/sds/request/cpm.py
@@ -1,7 +1,10 @@
 import copy
+import tornado
 
 from typing import List
 from lookup.sds_exception import SDSException
+from request.base_handler import ORG_CODE_QUERY_PARAMETER_NAME, ORG_CODE_FHIR_IDENTIFIER, \
+    IDENTIFIER_QUERY_PARAMETER_NAME, SERVICE_ID_FHIR_IDENTIFIER, PARTY_KEY_FHIR_IDENTIFIER
 
 async def get_device_from_cpm(ods_code: str, interaction_id: str, manufacturing_organization: str = None, party_key: str = None) -> List:
     return [
@@ -25,100 +28,46 @@ async def get_endpoint_from_cpm(ods_code: str, interaction_id: str = None, party
 def filter_cpm_devices_response(data: dict, query_parts: dict):
     if "org_code" not in query_parts or "interaction_id" not in query_parts or not query_parts["org_code"] or not query_parts["interaction_id"]:
             raise SDSException("org_code and interaction_id must be provided")
-
-    filtered_results = []
-    filters = {
-        "org_code": False,
-        "service_id": False,
+    
+    query_mapping = {
+        "org_code": "Owner",
+        "interaction_id": "InteractionIds",
+        "manufacturing_organization": "ManufacturingOdsCode",
+        "party_key": "PartyKey"
     }
-    if "manufacturing_organization" in query_parts:
-        filters["manufact_org"] = False
-    if "party_key" in query_parts:
-        filters["party_key"] = False
-        
-    for result in data["entry"]:
-        if "resourceType" in result and result["resourceType"] == "Bundle":
-            for index, res in enumerate(result["entry"]):
-                if "resourceType" in res and res["resourceType"] == "QuestionnaireResponse":
-                    for service in res["item"]:
-                        if service["text"] == "Owner":
-                            for answer in service["answer"]:
-                                if answer["valueString"] == query_parts["org_code"]:
-                                    filters["org_code"] = True
-                        if service["text"] == "InteractionIds":
-                            for answer in service["answer"]:
-                                if answer["valueString"] == query_parts["interaction_id"]:
-                                    filters["service_id"] = True
-                                    break
-                        if "manufacturing_organization" in query_parts and service["text"] == "ManufacturingOdsCode":
-                            for answer in service["answer"]:
-                                if answer["valueString"] == query_parts["manufacturing_organization"]:
-                                    filters["manufact_org"] = True
-                                    break
-                        if "party_key" in query_parts and service["text"] == "PartyKey":
-                            for answer in service["answer"]:
-                                if answer["valueString"] == query_parts["party_key"]:
-                                    filters["party_key"] = True
-                                    break
-        all_filters_true = all(value for value in filters.values())
-        if all_filters_true:
-            filtered_results.append(result["entry"])
-        filters = {key: False for key in filters}
-                    
-    return filtered_results
+    return filter_cpm_response(data, query_parts, query_mapping)
 
 def filter_cpm_endpoints_response(data: dict, query_parts: dict):
     non_empty_count = sum(1 for value in query_parts.values() if value and value != 0)
     if non_empty_count < 2:
-        self._raise_invalid_query_params_error()
+        _raise_invalid_query_params_error()
 
+    query_mapping = {
+        "org_code": "managingOrganization",
+        "service_id": "interactionID",
+        "party_key": "mhsPartyKey"
+    }
+    return filter_cpm_response(data, query_parts, query_mapping)
+
+def filter_cpm_response(data: dict, query_parts: dict, query_mapping: dict):
     filtered_results = []
-    # All the filters are optional but at least 2 should exist..
-    filters = {}
-    if "org_code" in query_parts:
-        filters["org_code"] = False
-    if "service_id" in query_parts:
-        filters["service_id"] = False
-    if "party_key" in query_parts:
-        filters["party_key"] = False
-        
+    filters = {key: False for key in query_parts}
+    
     for result in data["entry"]:
         if "resourceType" in result and result["resourceType"] == "Bundle":
             for index, res in enumerate(result["entry"]):
-                if "resourceType" in res and res["resourceType"] == "Device":
-                    if "org_code" in query_parts:
-                        if "owner" in res:
-                            if res["owner"]["identifier"]["value"] == query_parts["org_code"]:
-                                filters["org_code"] = True
-                                
-                        
                 if "resourceType" in res and res["resourceType"] == "QuestionnaireResponse":
                     for service in res["item"]:
-                        # if service["text"] == "Owner":
-                        #     for answer in service["answer"]:
-                        #         if answer["valueString"] == query_parts["org_code"]:
-                        #             filters["org_code"] = True
-                        # if service["text"] == "InteractionIds":
-                        #     for answer in service["answer"]:
-                        #         if answer["valueString"] == query_parts["interaction_id"]:
-                        #             filters["service_id"] = True
-                        #             break
-
-                        if "service_id" in query_parts and service["text"] == "interactionID":
-                            for answer in service["answer"]:
-                                if answer["valueString"] == query_parts["service_id"]:
-                                    filters["service_id"] = True
-                                    break
-                        if "party_key" in query_parts and service["text"] == "PartyKey":
-                            for answer in service["answer"]:
-                                if answer["valueString"] == query_parts["party_key"]:
-                                    filters["party_key"] = True
-                                    break
+                        for key, value in query_parts.items():
+                            if service["text"] == query_mapping[key]:
+                                for answer in service["answer"]:
+                                    if answer["valueString"] == value:
+                                        filters[key] = True                   
         all_filters_true = all(value for value in filters.values())
         if all_filters_true:
             filtered_results.append(result["entry"])
         filters = {key: False for key in filters}
-                    
+    
     return filtered_results
 
 def transform_device_to_SDS(data: List) -> List:
@@ -167,3 +116,18 @@ def process_questionnaire_response(item, data_dict):
                 data_dict["nhsAsClient"] = [service["answer"][0].get("valueString", "")]
                 data_dict["nhsIdCode"] = service["answer"][0].get("valueString", "")
     return data_dict
+
+def _raise_invalid_query_params_error():
+        org_code = f'{ORG_CODE_QUERY_PARAMETER_NAME}={ORG_CODE_FHIR_IDENTIFIER}|value'
+        party_key = f'{IDENTIFIER_QUERY_PARAMETER_NAME}={PARTY_KEY_FHIR_IDENTIFIER}|value'
+        service_id = f'{IDENTIFIER_QUERY_PARAMETER_NAME}={SERVICE_ID_FHIR_IDENTIFIER}|value'
+
+        raise tornado.web.HTTPError(
+            status_code=400,
+            log_message=f"Missing or invalid query parameters. "
+                        f"Should one of following combinations: ["
+                        f"'{org_code}&{service_id}&{party_key}'"
+                        f"'{org_code}&{service_id}'"
+                        f"'{org_code}&{party_key}'"
+                        f"'{service_id}&{party_key}'"
+                        "]")
