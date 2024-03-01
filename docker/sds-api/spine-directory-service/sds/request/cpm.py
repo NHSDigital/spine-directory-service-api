5620d9517584ae943fed221c4ccfd53975bc3cb4
jameslinnell <james.linnell2@nhs.net>
2024-03-01 13:06:45 +0000

    client_id

--- docker/sds-api/spine-directory-service/sds/request/cpm.py
+++ docker/sds-api/spine-directory-service/sds/request/cpm.py
@@ -56,7 +56,7 @@ def make_get_request(call_name: str, url, headers=None, params=None):
     return res
 
 def handle_error(response, call_name):
-    if response.status_code != 200 and response.status_code != 404:
+    if response.status_code != 200 or response.status_code != 404:
         detail = f"Request to {call_name} failed with message: {response.text}"
         logger.info(detail)
         raise SDSException(detail)
