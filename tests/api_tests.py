34075582b70bd6699ee5474b6382715fde314829
jameslinnell <james.linnell2@nhs.net>
2024-03-20 11:05:12 +0000

    refactor code

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -340,16 +340,16 @@ async def test_endpoints(test_app, api_client: APISessionClient, request_data):
 
     query_params = request_data['query_params']
     uri = _build_test_path(request_data['endpoint'], query_params)
-    await _assert_response(uri, headers, request_data['result_count'], request_data['status_code'])
+    await _assert_response(api_client, uri, headers, request_data['result_count'], request_data['status_code'])
     
     # Re-run with use_cpm as a query
     query_params_cpm = request_data['query_params']
     query_params_cpm['use_cpm'] = USE_CPM_ARGUMENT
     uri_cpm = _build_test_path(request_data['endpoint'], query_params=query_params_cpm)
-    await _assert_response(uri_cpm, headers, request_data['result_count'], request_data['status_code'])
+    await _assert_response(api_client, uri_cpm, headers, request_data['result_count'], request_data['status_code'])
 
        
-async def _assert_response(uri, headers, result_count, expected_status):
+async def _assert_response(api_client, uri, headers, result_count, expected_status):
     async with api_client.get(
         uri,
         headers=headers,
