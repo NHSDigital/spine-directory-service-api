92e4e4c5ad95514886bd0c1513c37cefe4d89f9a
jameslinnell <james.linnell2@nhs.net>
2024-03-26 10:35:55 +0000

    Remove unused params

--- docker/sds-api/spine-directory-service/sds/request/cpm.py
+++ docker/sds-api/spine-directory-service/sds/request/cpm.py
@@ -55,10 +55,10 @@ def process_cpm_device_request(data: dict, query_parts: dict):
 
 def make_get_request(call_name: str, url, headers=None, params=None):
     res = requests.get(url, headers=headers, params=params)
-    handle_error(res, call_name, url, headers, params)
+    handle_error(res, call_name)
     return res
 
-def handle_error(response, call_name, url, headers, params):
+def handle_error(response, call_name):
     if response.status_code != 200:
         detail = f"Request to {call_name} failed with message: {response.text}"
         logger.info(detail)
