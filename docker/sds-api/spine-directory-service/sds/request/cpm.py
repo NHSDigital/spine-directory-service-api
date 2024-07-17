75e0cee90a502bd53a1ddb210c9c2258b87bface
jameslinnell <james.linnell2@nhs.net>
2024-07-17 14:25:15 +0100

    Remove filtering
    version 2 of cpm search
    
    version 2 of cpm search for endpoint
    
    version 2 of cpm search for endpoint
    
    version 2 of cpm search for endpoint
    
    version 2 of cpm search for endpoint
    
    SonarCloud
    
    SonarCloud
    
    SonarCloud
    
    map filters to cpm expected
    
    map filters to cpm expected
    
    Change endpoint
    
    minor changes
    
    minor changes
    
    minor changes
    
    Fix loop if no results
    
    Update mocks to contain correct payload
    
    remove tests that will not work
    
    remove tests that will not work
    
    Mock USE_CPM in unit tests
    
    Update test expected data
    
    Change Device and Endpoint Client class names
    
    Change Device and Endpoint Client class names

--- docker/sds-api/spine-directory-service/sds/request/cpm.py
+++ docker/sds-api/spine-directory-service/sds/request/cpm.py
@@ -1,14 +1,15 @@
 import os
+import json
 import copy
 import tornado
 import requests
 from tornado.web import RequestHandler
 
-from typing import List
+from typing import List, Dict
 from lookup.sds_exception import SDSException
 from request.base_handler import CPM_FILTER, CPM_FILTER_IDENTIFIER, ORG_CODE_QUERY_PARAMETER_NAME, ORG_CODE_FHIR_IDENTIFIER, \
     IDENTIFIER_QUERY_PARAMETER_NAME, SERVICE_ID_FHIR_IDENTIFIER, PARTY_KEY_FHIR_IDENTIFIER
-from request.cpm_config import DEVICE_FILTER_MAP, ENDPOINT_FILTER_MAP, DEVICE_DATA_MAP, ENDPOINT_DATA_MAP, DEFAULT_ENDPOINT_DICT, DEFAULT_DEVICE_DICT
+from request.cpm_config import DEVICE_DATA_MAP, ENDPOINT_DATA_MAP, DEFAULT_ENDPOINT_DICT, DEFAULT_DEVICE_DICT, FILTER_MAP_DEVICE, FILTER_MAP_ENDPOINT
 from utilities.constants import INTERACTION_MAPPINGS, RELIABLE_SERVICES
 from utilities import config
 from utilities import integration_adaptors_logger as log
@@ -36,10 +37,9 @@ async def get_device_from_cpm(tracking_id_headers: dict, **query_parts) -> List:
         apigee_url = f"{os.environ['APIGEE_URL']}/{os.environ['CPM_PATH_URL']}"
     except KeyError as e:
         raise KeyError(f"Environment variable is required {e}")
-    cpm_client = CpmClient(client_id=client_id, apigee_url=apigee_url, endpoint="product")
+    cpm_client = DeviceCpmClient(client_id=client_id, apigee_url=apigee_url, query_params=query_parts)
     data = await cpm_client.get_cpm(extra_headers=tracking_id_headers)
-
-    return process_cpm_device_request(data=data, query_parts=query_parts)
+    return process_cpm_device_request(data=data)
 
 
 async def get_endpoint_from_cpm(tracking_id_headers: dict, **query_parts) -> List:
@@ -48,24 +48,90 @@ async def get_endpoint_from_cpm(tracking_id_headers: dict, **query_parts) -> Lis
         apigee_url = f"{os.environ['APIGEE_URL']}/{os.environ['CPM_PATH_URL']}"
     except KeyError as e:
         raise KeyError(f"Environment variable is required {e}")
-    cpm_client = CpmClient(client_id=client_id, apigee_url=apigee_url, endpoint="endpoint")
+    cpm_client = EndpointCpmClient(client_id=client_id, apigee_url=apigee_url, query_params=query_parts)
     data = await cpm_client.get_cpm(extra_headers=tracking_id_headers)
+    ldap_results = process_cpm_endpoint_request(data=data)
+    mhs_converted = await set_mhs_endpoint(ldap_results=ldap_results, tracking_id_headers=tracking_id_headers)
+    return mhs_converted
 
-    return process_cpm_endpoint_request(data=data, query_parts=query_parts)
 
-def process_cpm_endpoint_request(data: dict, query_parts: dict):
-    endpoints = EndpointCpm(data=data, query_parts=query_parts)
-    filtered_endpoints = endpoints.filter_cpm_response()
-    ldap_converted = endpoints.transform_to_ldap(filtered_endpoints)
-    return endpoints.set_mhs_endpoint(ldap_converted)
+async def set_mhs_endpoint(ldap_results: list, tracking_id_headers: dict):
+    if ldap_results:
+        for key, ldap_result in enumerate(ldap_results):
+            service, interaction = _extract_service_and_interaction(ldap_result['nhsMhsSvcIA'])
+            if service in RELIABLE_SERVICES:
+                address: Optional[str] = None
+                for core_spine_interaction, interactions in INTERACTION_MAPPINGS.items():
+                    if interaction in interactions:
+                        address = _get_address(core_spine_interaction, tracking_id_headers)
+                        break
 
-def process_cpm_device_request(data: dict, query_parts: dict):
-    devices = DeviceCpm(data=data, query_parts=query_parts)
-    filtered_devices = devices.filter_cpm_response()
-    return devices.transform_to_ldap(filtered_devices)
+                if address:
+                    ldap_results[key]['nhsMHSEndPoint'] = address
+
+    return ldap_results
+
+def _extract_service_and_interaction(service_interaction: str):
+    if not service_interaction:
+        return False
+
+    parts = service_interaction.split(':')
+
+    if len(parts) < 2:
+        raise RuntimeError(f"Invalid service interaction: {service_interaction}")
+
+    interaction_part = parts[-1]
+    service_part = parts[-2]
+
+    return service_part, interaction_part
+
+async def _get_address(service_id: str, tracking_id_headers: dict) -> str:
+    spine_core_ods_code = config.get_config('SPINE_CORE_ODS_CODE')
+    logger.info("Looking up forward reliable/express routing and reliability information. {org_code}, {service_id}",
+                fparams={"org_code": spine_core_ods_code, "service_id": service_id})
+    query_params = {
+        "org_code": spine_core_ods_code,
+        "interaction_id": service_id
+    }
+    
+    try:
+        client_id = os.environ["CPM_CLIENT_KEY"]
+        apigee_url = f"{os.environ['APIGEE_URL']}/{os.environ['CPM_PATH_URL']}"
+    except KeyError as e:
+        raise KeyError(f"Environment variable is required {e}")
+    
+    cpm_client = EndpointCpmClient(client_id=client_id, apigee_url=apigee_url, query_params=query_params)
+    data = await cpm_client.get_cpm(extra_headers=tracking_id_headers)
+    
+    address_endpoint = EndpointCpm(data=data)
+    ldap_address = address_endpoint.transform_to_ldap()
+
+    address = []
+    try:
+        if len(ldap_address) == 1:
+            address = ldap_address[0] if len(ldap_address) == 1 else self._raise_value_error("result", ldap_address)
+            return address.get('nhsMHSEndPoint') if len(address['nhsMHSEndPoint']) == 1 else _raise_value_error("address", address)
+    except IndexError:
+        _raise_index_error("result", ldap_address)
+
+def _raise_value_error(self, message: str, address: List = []):
+    raise ValueError(f"Expected 1 {message} for forward reliable/express routing and reliability but got {str(len(address))}")
+
+def _raise_index_error(self, message: str, address: List = []):
+    raise IndexError(f"Expected 1 {message} for forward reliable/express routing and reliability but got {str(len(address))}")
+    
+def process_cpm_endpoint_request(data: dict):
+    endpoints = EndpointCpm(data=data)
+    ldap_converted = endpoints.transform_to_ldap()
+    return ldap_converted
+
+def process_cpm_device_request(data: dict):
+    devices = DeviceCpm(data=data)
+    ldap_converted = devices.transform_to_ldap()
+    return ldap_converted
 
 def make_get_request(call_name: str, url, headers=None, params=None):
-    res = requests.get(url, headers=headers, params=params)
+    res = requests.get(url, params=params, headers=headers)
     handle_error(res, call_name)
     return res
 
@@ -76,76 +142,90 @@ def handle_error(response, call_name):
         raise SDSException(detail)
 
 class CpmClient:
-    def __init__(self, client_id: str, apigee_url: str,  endpoint: str) -> None:
+    FILTER_MAP = {}
+    def __init__(self, client_id: str, apigee_url: str, query_params: dict) -> None:
         self._client_id = client_id
         self._apigee_url = apigee_url
-        self._endpoint = endpoint
-
+        self._params = self._set_params(query_params)
+        
     async def get_cpm(self, extra_headers: dict):
         logger.info("Contacting CPM")
         url = f"https://{self._apigee_url}"
-        search_endpoint = f"Device?device_type={self._endpoint}&use_mock=true"
+        search_endpoint = self._endpoint
         headers = {
             'version': '1',
             'Authorization': 'letmein',
             'Content-Type': 'application/json',
-            'apiKey': self._client_id,
+            'apikey': self._client_id,
             **extra_headers
         }
-        params = {}
-        logger.info("Requesting data from... {url}/{endpoint}", fparams={"url": url, "endpoint": search_endpoint})
-        res = make_get_request(call_name="SDS get_cpm", url=f"{url}/{search_endpoint}", headers=headers, params=params)
+        logger.info("Requesting data from... {url}/{endpoint}", fparams={"url": url, "endpoint": search_endpoint,  "query_params": self._params})
+        res = make_get_request(call_name="SDS get_cpm", url=f"{url}/{search_endpoint}", headers=headers, params=self._params)
         return self._get_response(res=res)
+
+    def _set_params(self, query_params: Dict[str, str]) -> Dict[str, str]:
+        params = {self.FILTER_MAP.get(key, key): value for key, value in query_params.items() if value is not None}
+        return params
 
     def _get_response(self, res):
         return res.json()
 
+class DeviceCpmClient(CpmClient):
+    FILTER_MAP = FILTER_MAP_DEVICE
+    def __init__(self, client_id: str, apigee_url: str, query_params: dict) -> None:
+        self.validate_filters(query_params)
+        self._endpoint = "searchSdsDevice"
+        super().__init__(client_id, apigee_url, query_params)
+
+    def validate_filters(self, query_params):
+        allowed_filters = ["org_code", "interaction_id", "manufacturing_organization", "party_key"]
+        for key, value in query_params.items():
+            if key not in allowed_filters:
+                raise SDSException(f"{key} not allowed in filters")
+
+        if "org_code" not in query_params or "interaction_id" not in query_params or not query_params["org_code"] or not query_params["interaction_id"]:
+            raise SDSException("org_code and interaction_id must be provided")
+
+
+class EndpointCpmClient(CpmClient):
+    FILTER_MAP = FILTER_MAP_ENDPOINT
+    def __init__(self, client_id: str, apigee_url: str, query_params: dict) -> None:
+        self.validate_filters(query_params)
+        self._endpoint = "searchSdsEndpoint"
+        super().__init__(client_id, apigee_url, query_params)
+
+    def validate_filters(self, query_params):
+        allowed_filters = ["org_code", "interaction_id", "party_key"]
+        for key, value in query_params.items():
+            if key not in allowed_filters:
+                raise SDSException(f"{key} not allowed in filters")
+
+        non_empty_count = sum(1 for value in query_params.values() if value and value != 0)
+        if non_empty_count < 2:
+            self._raise_invalid_query_params_error()
+    
+    def _raise_invalid_query_params_error(self):
+        org_code = f'{ORG_CODE_QUERY_PARAMETER_NAME}={ORG_CODE_FHIR_IDENTIFIER}|value'
+        party_key = f'{IDENTIFIER_QUERY_PARAMETER_NAME}={PARTY_KEY_FHIR_IDENTIFIER}|value'
+        service_id = f'{IDENTIFIER_QUERY_PARAMETER_NAME}={SERVICE_ID_FHIR_IDENTIFIER}|value'
+        log_message=f"Missing or invalid query parameters. Should one of following combinations: ['{org_code}&{service_id}&{party_key}','{org_code}&{service_id}','{org_code}&{party_key}','{service_id}&{party_key}']"
+        raise SDSException(log_message)
+
 class BaseCpm:
-    FILTER_MAP = {}
     DATA_MAP = {}
     DEFAULT_DICT = {}
 
-    def __init__(self, data, query_parts):
+    def __init__(self, data):
         self.data = data
-        self.query_parts = query_parts
-
-    def filter_cpm_response(self):
-        filtered_results = []
-        filters = {key: False for key, value in self.query_parts.items() if value is not None}
-        for result in self.data["entry"]:
-            for index, res in enumerate(result["entry"]) if result.get("resourceType") == "Bundle" else []:
-                for service in res["item"] if res.get("resourceType") == "QuestionnaireResponse" else []:
-                    filters = self._check_each_item(self, filters, service)
-
-            all_filters_true = all(filters.values())
-            if all_filters_true:
-                filtered_results.append(result["entry"])
-            filters = {key: False for key in filters}
-
-        return filtered_results
-
-    @staticmethod
-    def _check_each_item(self, filters, service):
-        for key, value in self.query_parts.items():
-            if service["text"] == self.FILTER_MAP[key]:
-                match = self._check_match(self, service["answer"], value)
-                if match:
-                    filters[key] = match
-        return filters
-
-    @staticmethod
-    def _check_match(self, answers, match):
-        return any(answer["valueString"] == match for answer in answers)
 
     @staticmethod
     def process_questionnaire_response(self, item, data_dict, ldap_data_mapping):
-        for service in item.get("item", []):
-            if service["text"] in ldap_data_mapping:
-                for answer in service["answer"]:
-                    key = ldap_data_mapping[service["text"]]
-                    value = answer.get("valueInteger", answer.get("valueString"))
-                    data_dict = self.set_data(data_dict, key, value)
-
+        for key, service in item.items():
+            if key in ldap_data_mapping:
+                for answer in service:
+                    new_key = ldap_data_mapping[key]
+                    data_dict = self.set_data(data_dict, new_key, answer)
+                    
         return data_dict
 
     @staticmethod
@@ -157,116 +237,33 @@ class BaseCpm:
 
         return data_dict
 
-    def transform_to_ldap(self, data: List) -> List:
+    def transform_to_ldap(self) -> List:
         ldap_data = []
-
+        data = self.data
         for d in data:
+            questionnaire_responses = d["questionnaire_responses"]
             data_dict = copy.deepcopy(self.DEFAULT_DICT)
-            for item in d:
-                if item.get("resourceType") == "QuestionnaireResponse":
-                    data_dict = self.process_questionnaire_response(self, item, data_dict, self.DATA_MAP)
-
-            ldap_data.append(data_dict)
+            for key, qr in questionnaire_responses.items():
+                for qr_key, qr_response in qr.items():
+                    responses = qr_response.get("answers", [])
+                    for resp in responses:
+                        data_dict = self.process_questionnaire_response(self, resp, data_dict, self.DATA_MAP)
+                    ldap_data.append(data_dict)
 
         return ldap_data
 
 
+class DeviceCpm(BaseCpm):
+    DATA_MAP = DEVICE_DATA_MAP
+    DEFAULT_DICT = DEFAULT_DEVICE_DICT
+    
+    def __init__(self, data):
+        super().__init__(data)
+    
+
 class EndpointCpm(BaseCpm):
-    FILTER_MAP = ENDPOINT_FILTER_MAP
     DATA_MAP = ENDPOINT_DATA_MAP
     DEFAULT_DICT = DEFAULT_ENDPOINT_DICT
 
-    def __init__(self, data, query_parts):
-        self.validate_filters(query_parts)
-        super().__init__(data, query_parts)
-
-    def validate_filters(self, query_parts):
-        non_empty_count = sum(1 for value in query_parts.values() if value and value != 0)
-        if non_empty_count < 2:
-            self._raise_invalid_query_params_error()
-
-    def set_mhs_endpoint(self, ldap_results):
-        for key, ldap_result in enumerate(ldap_results):
-            service, interaction = self._extract_service_and_interaction(ldap_result['nhsMhsSvcIA'])
-            if service in RELIABLE_SERVICES:
-                address: Optional[str] = None
-                for core_spine_interaction, interactions in INTERACTION_MAPPINGS.items():
-                    if interaction in interactions:
-                        address = self._get_address(core_spine_interaction)
-                        break
-
-                if address:
-                    ldap_results[key]['nhsMHSEndPoint'] = address
-
-        return ldap_results
-
-    @staticmethod
-    def _extract_service_and_interaction(service_interaction: str):
-        if not service_interaction:
-            return False
-
-        parts = service_interaction.split(':')
-
-        if len(parts) < 2:
-            raise RuntimeError(f"Invalid service interaction: {service_interaction}")
-
-        interaction_part = parts[-1]
-        service_part = parts[-2]
-
-        return service_part, interaction_part
-
-    def _get_address(self, service_id: str) -> str:
-        spine_core_ods_code = config.get_config('SPINE_CORE_ODS_CODE')
-        logger.info("Looking up forward reliable/express routing and reliability information. {org_code}, {service_id}",
-                    fparams={"org_code": spine_core_ods_code, "service_id": service_id})
-        query_params = {
-            "org_code": spine_core_ods_code,
-            "interaction_id": service_id
-        }
-        address_endpoint = EndpointCpm(data=self.data, query_parts=query_params)
-        filtered_address_endpoint = address_endpoint.filter_cpm_response()
-        ldap_address = address_endpoint.transform_to_ldap(filtered_address_endpoint)
-
-        address = []
-        try:
-            if len(ldap_address) == 1:
-                address = ldap_address[0] if len(ldap_address) == 1 else self._raise_value_error("result", ldap_address)
-                return address.get('nhsMHSEndPoint') if len(address['nhsMHSEndPoint']) == 1 else self._raise_value_error("address", address)
-        except IndexError:
-            self._raise_index_error("result", ldap_address)
-
-    def _raise_value_error(self, message: str, address: List = []):
-        raise ValueError(f"Expected 1 {message} for forward reliable/express routing and reliability but got {str(len(address))}")
-
-    def _raise_index_error(self, message: str, address: List = []):
-        raise IndexError(f"Expected 1 {message} for forward reliable/express routing and reliability but got {str(len(address))}")
-
-
-    def _raise_invalid_query_params_error(self):
-        org_code = f'{ORG_CODE_QUERY_PARAMETER_NAME}={ORG_CODE_FHIR_IDENTIFIER}|value'
-        party_key = f'{IDENTIFIER_QUERY_PARAMETER_NAME}={PARTY_KEY_FHIR_IDENTIFIER}|value'
-        service_id = f'{IDENTIFIER_QUERY_PARAMETER_NAME}={SERVICE_ID_FHIR_IDENTIFIER}|value'
-
-        raise tornado.web.HTTPError(
-            status_code=400,
-            log_message=f"Missing or invalid query parameters. "
-                        f"Should one of following combinations: ["
-                        f"'{org_code}&{service_id}&{party_key}'"
-                        f"'{org_code}&{service_id}'"
-                        f"'{org_code}&{party_key}'"
-                        f"'{service_id}&{party_key}'"
-                        "]")
-
-
-class DeviceCpm(BaseCpm):
-    FILTER_MAP = DEVICE_FILTER_MAP
-    DATA_MAP = DEVICE_DATA_MAP
-    DEFAULT_DICT = DEFAULT_DEVICE_DICT
-
-    def __init__(self, data, query_parts):
-        self.validate_filters(query_parts)
-        super().__init__(data, query_parts)
-
-    def validate_filters(self, query_parts):
-        if "org_code" not in query_parts or "interaction_id" not in query_parts or not query_parts["org_code"] or not query_parts["interaction_id"]:
-            raise SDSException("org_code and interaction_id must be provided")
+    def __init__(self, data):
+        super().__init__(data)
