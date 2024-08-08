ac6c90adb0cedae3df8ad513a529dec04c14f039
jameslinnell <james.linnell2@nhs.net>
2024-08-08 14:22:16 +0100

    check os key exists

--- docker/sds-api/spine-directory-service/sds/request/cpm.py
+++ docker/sds-api/spine-directory-service/sds/request/cpm.py
@@ -17,7 +17,12 @@ logger = log.IntegrationAdaptorsLogger(__name__)
 
 
 def should_use_cpm(handler: RequestHandler) -> bool:
-    if os.environ["USE_CPM"] == "1":
+    try: 
+        use_cpm = os.environ["USE_CPM"]
+    except KeyError as e:
+        raise KeyError(f"Environment variable is required {e}")
+    
+    if use_cpm == "1":
         return True
     return (
         handler.get_query_argument(name=CPM_FILTER, default=None)
