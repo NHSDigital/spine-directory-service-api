288aae732550ad4c8270d98b841438bf920912cb
jameslinnell <james.linnell2@nhs.net>
2024-08-08 16:10:51 +0100

    mock env var

--- docker/sds-api/spine-directory-service/sds/request/tests/test_accredited_system_handler.py
+++ docker/sds-api/spine-directory-service/sds/request/tests/test_accredited_system_handler.py
@@ -1,6 +1,7 @@
 from os import path
 from request.tests.request_handler_test_base import RequestHandlerTestBase, ORG_CODE, SERVICE_ID, PARTY_KEY, MANUFACTURING_ORG
 from utilities import test_utilities
+from unittest.mock import patch
 
 EXPECTED_SINGLE_DEVICE_JSON_FILE_PATH = path.join(path.dirname(__file__), "examples/single_device.json")
 EXPECTED_MULTIPLE_DEVICES_JSON_FILE_PATH = path.join(path.dirname(__file__), "examples/multiple_devices.json")
@@ -28,6 +29,7 @@ MULTIPLE_ACCREDITED_SYSTEM_DETAILS.append({
 
 class TestAccreditedSystemHandler(RequestHandlerTestBase):
 
+    @patch.dict(os.environ, {"USE_CPM": "0"})
     def test_get_single_result(self):
         self.sds_client.get_as_details.return_value = test_utilities.awaitable(SINGLE_ACCREDITED_SYSTEM_DETAILS)
 
@@ -37,6 +39,7 @@ class TestAccreditedSystemHandler(RequestHandlerTestBase):
 
         self.sds_client.get_as_details.assert_called_with(ORG_CODE, SERVICE_ID, None, None)
 
+    @patch.dict(os.environ, {"USE_CPM": "0"})
     def test_get_multiple_results(self):
         self.sds_client.get_as_details.return_value = test_utilities.awaitable(MULTIPLE_ACCREDITED_SYSTEM_DETAILS)
 
@@ -46,6 +49,7 @@ class TestAccreditedSystemHandler(RequestHandlerTestBase):
 
         self.sds_client.get_as_details.assert_called_with(ORG_CODE, SERVICE_ID, None, None)
 
+    @patch.dict(os.environ, {"USE_CPM": "0"})
     def test_correlation_id_is_set_as_response_header(self):
         def mock200():
             self.sds_client.get_as_details.return_value = test_utilities.awaitable(SINGLE_ACCREDITED_SYSTEM_DETAILS)
@@ -60,6 +64,7 @@ class TestAccreditedSystemHandler(RequestHandlerTestBase):
             mock500
         )
 
+    @patch.dict(os.environ, {"USE_CPM": "0"})
     def test_get_returns_error(self):
         with self.subTest("Lookup error"):
             self.sds_client.get_as_details.side_effect = Exception("some error")
@@ -67,6 +72,7 @@ class TestAccreditedSystemHandler(RequestHandlerTestBase):
             self.assertEqual(response.code, 500)
             super()._assert_500_operation_outcome(response.body.decode())
 
+    @patch.dict(os.environ, {"USE_CPM": "0"})
     def test_get_handles_missing_params(self):
         with self.subTest("Missing Org Code"):
             response = self.fetch(self._build_device_url(org_code=None, service_id=SERVICE_ID), method="GET")
@@ -116,10 +122,12 @@ class TestAccreditedSystemHandler(RequestHandlerTestBase):
                     response.body.decode(),
                     f"HTTP 400: Bad Request (Missing or invalid 'manufacturing-organization' query parameter. Should be 'manufacturing-organization=https://fhir.nhs.uk/Id/ods-organization-code|value')")
 
+    @patch.dict(os.environ, {"USE_CPM": "0"})
     def test_get_handles_different_accept_header(self):
         self.sds_client.get_as_details.return_value = test_utilities.awaitable(SINGLE_ACCREDITED_SYSTEM_DETAILS)
         super()._test_get_handles_different_accept_header(
             super()._build_device_url(), EXPECTED_SINGLE_DEVICE_JSON_FILE_PATH)
 
+    @patch.dict(os.environ, {"USE_CPM": "0"})
     def test_should_return_405_when_using_non_get(self):
         super()._test_should_return_405_when_using_non_get(super()._build_device_url())
